0e231db44274619800f9da3e39e1f8d2d52aeed2
Pavlo Morozov <pavelmorozov@mail.ru>
2014-09-02 15:02:39 +0300

    First version for speed testing.

--- src/fbp/image/processing/FileProcessor.java
+++ src/fbp/image/processing/FileProcessor.java
@@ -1,21 +1,91 @@
 package fbp.image.processing;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FileProcessor {
+	
+	private String workDirectory;
+	private static final String OUTPUT = "output";
+	
 	FileProcessor(){
 		
 	}
 	
-	public Set<String> listFiles(String initialFolder){
-		System.out.println("Working Directory: " + 
-				System.getProperty("user.dir"));    	
+	public String getOutputFolderName(){
+		return OUTPUT;
+	}
+	
+	public Set<File> listFiles(String initialFolder){
+		//########## Here change to initialFolder ##########  
+		workDirectory = System.getProperty("user.dir");
 		
-		Set<String> fileSet = new HashSet<String>();
-		fileSet.add("1.original.jpg");		
-		fileSet.add("2.original.jpg");
+		System.out.println("Working Directory: " + 
+				workDirectory);
 
+		System.out.println("Create folder: " + 
+				OUTPUT);
+		boolean created = (new File(workDirectory+'/'+OUTPUT)).mkdirs();
+		if (!created) System.out.println("Created sucessfully");
+		else System.out.println("Folder not created. May be exist?");
+
+	    // create a file that is really a directory
+	    File aDirectory = new File(workDirectory);
+	    
+	    FilenameFilter fileNameFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+               if(name.lastIndexOf('.')>0)
+               {
+                  // get last index for '.' char
+                  int lastIndex = name.lastIndexOf('.');
+                  
+                  // get extension
+                  String str = name.substring(lastIndex);
+                  
+                  // match path name extension
+                  if(str.equals(".jpg"))
+                  {
+                     return true;
+                  }
+                  // match path name extension
+                  if(str.equals(".jpeg"))
+                  {
+                     return true;
+                  }
+                  if(str.equals(".gif"))
+                  {
+                     return true;
+                  }
+                  if(str.equals(".png"))
+                  {
+                     return true;
+                  }
+                  if(str.equals(".tiff"))
+                  {
+                     return true;
+                  }                  
+               }
+               return false;
+            }
+         };
+         
+         // returns pathnames for files and directory
+         File[] filesInDir = aDirectory.listFiles(fileNameFilter);	    
+	    
+	    // get a listing of all files in the directory	    
+		Set<File> fileSet = new HashSet<File> (Arrays.asList(filesInDir));;
+//		fileSet.add("1.original.jpg");		
+//		fileSet.add("2.original.jpg");
+//		fileSet.add("1.jpg");
+//		fileSet.add("2.jpg");
+		
+		System.out.println("found files:"+ fileSet.size());
+		
 		return fileSet;
 	}  
 
--- src/fbp/image/processing/Starter.java
+++ src/fbp/image/processing/Starter.java
@@ -17,10 +17,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
 //import javax.swing.text.Position;
+
 
 
 
@@ -44,19 +46,16 @@ public class Starter {
     	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss:SSS");
     	System.out.println("######### File conversaion #########");
     	System.out.println(sdf.format(cal.getTime()) +" - Started" );
+
+    	Set<BufferedImage> sourceImageSet = new HashSet<BufferedImage>();    	
     	
     	FileProcessor fileProcessor = new FileProcessor();  
-    	Set<String> imageSet = fileProcessor.listFiles("");  
-    	
-		InputStream inputStream;
-		OutputStream outputStream;
+    	Set<File> imageSet = fileProcessor.listFiles("");
 		
-		String watermark = "- < Test watermark > -";
-		
-		for (String fileName:imageSet){
-			String exportFileName = "export_"+fileName;
+		for (File imageFile:imageSet){
+			//String exportFileName = "export_"+imageFile.getName();
 			
-			BufferedImage sourceImage = ImageIO.read(new File(fileName));
+			BufferedImage sourceImage = ImageIO.read(imageFile);
 			int imageWidth = sourceImage.getWidth();
 			int imageHeight = sourceImage.getHeight();
 			
@@ -89,162 +88,29 @@ public class Starter {
 	        Shape textShape = fontGV.getOutline();
 	        double textWidth = size.getWidth();
 	        double textHeight = size.getHeight();
-	        AffineTransform rotate45 = AffineTransform.getRotateInstance(Math.PI / 5d);
+	        AffineTransform rotate45 = AffineTransform.getRotateInstance(Math.PI / -5d);
 	        Shape rotatedText = rotate45.createTransformedShape(textShape);
+	        //Shape rotatedText = textShape;
 
-	        // use a gradient that repeats 4 times
-	        g2d.setPaint((Paint) new GradientPaint(0, 0,
-	                            new Color(0.5f, 0.5f, 0.5f, 0.5f),
-	                            destinationImage.getWidth() / 2, destinationImage.getHeight() / 2,
-	                            new Color(0.5f, 0.5f, 0.5f, 0.5f)));
-	        g2d.setStroke(new BasicStroke(0.5f));
-
-	        //double yStep = Math.sqrt(textWidth * textWidth / 2) + 20;
+	        g2d.setColor(new Color(120,120,120,70));
+	        //g2d.setStroke(new BasicStroke(1f));
 	        
-//	        // step in y direction is calc'ed using pythagoras + 5 pixel padding
-//	        double yStep = Math.sqrt(textWidth * textWidth / 2) + 20;
-//
-//	        // step over image rendering watermark text
-//	        for (double x = -textHeight * 3; x < destinationImage.getWidth(); x += (textHeight * 3)) {
-//	            double y = -yStep;
-//	            for (y = -yStep * 2 ; y < destinationImage.getHeight(); y += yStep) {
-//	                g2d.draw(rotatedText);
-//	                g2d.fill(rotatedText);
-//	                g2d.translate(0, yStep);
-//	            }
-//	            g2d.translate(textHeight * 3, -(y + yStep));
-//	        }			
-			
-	        ImageIO.write(destinationImage, "jpg", new File(exportFileName));
-			
-			
-			
-//	        Graphics2D g2d = (Graphics2D) destinationImage.getGraphics();
-//	        
-//	        // initializes necessary graphic properties
-//	        AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
-//	        g2d.setComposite(alphaChannel);
-//	        g2d.setColor(Color.GRAY);
-//	        g2d.setFont(new Font("Arial", Font.BOLD, 64));
-//	        
-//	        
-//	        
-//	        FontMetrics fontMetrics = g2d.getFontMetrics();
-//	        Rectangle2D rect = fontMetrics.getStringBounds(WATERMARK_TEXT, g2d);
-//	 
-//	        // calculates the coordinate where the String is painted
-//	        int centerX = (destinationImage.getWidth() - (int) rect.getWidth()) / 2;
-//	        int centerY = destinationImage.getHeight() / 2;
-//	 
-//	        // paints the textual watermark
-//	        g2d.drawString(WATERMARK_TEXT, centerX, centerY);
-//	 
-//	        //ImageIO.write(sourceImage, "jpg", destImageFile);
-//	        ImageIO.write(destinationImage, "jpg", new File(exportFileName));
-//	        g2d.dispose();
-//	 
-//	        //System.out.println("The tex watermark is added to the image.");			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-//			 Graphics2D graphics2D = (Graphics2D)destinationImage.getGraphics();
-//			 graphics2D.setFont(new Font("Arial", Font.BOLD, 30));
-//			 graphics2D.drawString("Watermarked!", destinationImage.getWidth()/2, destinationImage.getHeight() / 2);
-			 
-
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-//			// Set up the caption properties
-//			String caption = WATERMARK_TEXT;
-//			Font font = new Font("Monospaced", Font.PLAIN, 14);
-//			Color c = Color.black;
-//			Position position = Position.CENTER;
-//			int insetPixels = 0;
-//
-//			// Apply caption to the image
-//			Caption filter = new Caption(caption, font, c, position, insetPixels);
-//			BufferedImage captionedImage = filter.apply(originalImage);			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		
-//			Graphics2D g2d = destinationImage.createGraphics();
-//			Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 10);
-//	        GlyphVector fontGV = font.createGlyphVector(g2d.getFontRenderContext(), WATERMARK_TEXT);
-//	        Rectangle size = fontGV.getPixelBounds(g2d.getFontRenderContext(), 0, 0);
-//	        Shape textShape = fontGV.getOutline();
-//	        double textWidth = size.getWidth();
-//	        double textHeight = size.getHeight();
-//	        AffineTransform rotate45 = AffineTransform.getRotateInstance(Math.PI / 4d);
-//	        Shape rotatedText = rotate45.createTransformedShape(textShape);
-//	        
-//	        // use a gradient that repeats 4 times
-//	        g2d.setPaint(new GradientPaint(0, 0,
-//	                            new Color(0f, 0f, 0f, 0.1f),
-//	                            destinationImage.getWidth() / 2, destinationImage.getHeight() / 2,
-//	                            new Color(1f, 1f, 1f, 0.1f)));
-//	        g2d.setStroke(new BasicStroke(0.5f));
-//
-//	        // step in y direction is calc'ed using pythagoras + 5 pixel padding
-//	        double yStep = Math.sqrt(textWidth * textWidth / 2) + 5;
-//
-//	        // step over image rendering watermark text
-//	        for (double x = -textHeight * 3; x < destinationImage.getWidth(); x += (textHeight * 3)) {
-//	            double y = -yStep;
-//	            for (; y < destinationImage.getHeight(); y += yStep) {
-//	                g2d.draw(rotatedText);
-//	                g2d.fill(rotatedText);
-//	                g2d.translate(0, yStep);
-//	            }
-//	            g2d.translate(textHeight * 3, -(y + yStep));
-//	        }	        
-//	        
-//	        g2d
+	        double yStep = Math.sqrt(textWidth * textWidth / 2)/2;
+	        double xStep = textHeight * 3;
 	        
-	        
-	        
-	        
-	        
+	        // step over image rendering watermark text
+	        for (double x = -xStep ; x < destinationImage.getWidth(); x += xStep) {
+	            double y = -yStep;
+	            for (; y < destinationImage.getHeight(); y += yStep) {
+	                //g2d.draw(rotatedText);
+	                g2d.fill(rotatedText);
+	                g2d.translate(0, yStep);
+	            }
+	            g2d.translate(xStep*5, -(y + yStep));
+	        }
 			
-			
-			//ImageIO.write(destinationImage, "jpg", new File(exportFileName));
-			System.out.println("File exported: " + exportFileName);
+			ImageIO.write(destinationImage, "jpg", new File(fileProcessor.getOutputFolderName()+'/'+imageFile.getName()));
+			System.out.println("File exported: " + imageFile.getName());
 		}
 		cal = Calendar.getInstance();
 		System.out.println(sdf.format(cal.getTime()) +" - Finished" );
